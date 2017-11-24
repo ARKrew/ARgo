c1d95de776e29e27551dfd099e30fc182f710084
Grace Park <gracepark@github.com>
2017-11-24 13:56:11 -0800

    clean up

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
-// import React, { Component } from 'react';
-// import { AppRegistry } from 'react-native';
-// import { Provider } from 'react-redux';
-// import ReduxThunk from 'redux-thunk';
-// import { createStore, applyMiddleware } from 'redux';
-// import AppNavigator from './AppNavigator';
-// // import firebase from 'firebase';
-// // Middleware
-// import reducers from './reducers';
-//
-// class App extends Component {
-//
-//   render() {
-//     // Wire up redux-thunk
-//     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-//
-//     return (
-//       // STORE
-//       // Pass reducers through createStore call
-//       <Provider store={store}>
-//         <AppNavigator />
-//       </Provider>
-//     );
-//   }
-// }
-//
-// AppRegistry.registerComponent('ARgo', () => App);
-
 import React, { Component } from 'react';
 import { Provider, connect } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-// import firebase from 'firebase';
 import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import { addNavigationHelpers } from 'react-navigation';
