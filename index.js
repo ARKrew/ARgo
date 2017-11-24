9e50d17b41f5fabafafdd993cf1696dc00d09ae8
Grace Park <gracepark@github.com>
2017-11-23 21:52:11 -0800

    write to Firebase

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-// import { AppRegistry } from 'react-native';
-import './src/App';
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import LoginForm from './src/components/LoginForm';
+import Profile from './src/components/Profile';
+import { StackNavigator } from 'react-navigation'
+import * as firebase from 'firebase';
 
-// AppRegistry.registerComponent('ARgo', () => App);
+const firebaseConfig = {
+  apiKey: 'AIzaSyCUGO1U43_sMeTlVm9nl7ytwcILcrFsjOg',
+  authDomain: 'argo-4cbff.firebaseapp.com',
+  databaseURL: 'https://argo-4cbff.firebaseio.com',
+  projectId: 'argo-4cbff',
+  storageBucket: 'argo-4cbff.appspot.com',
+  messagingSenderId: '246167725320'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const RouteConfigs = {
+    Login: { screen: LoginForm },
+    Home: { screen: Profile }
+};
+
+const StackNavigatorConfig = {
+    headerMode: 'none',
+};
+
+const App = StackNavigator(RouteConfigs, StackNavigatorConfig);
+
+AppRegistry.registerComponent('ARgo', () => App);
--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
-import { Provider } from 'react-redux';
-import ReduxThunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-import AppNavigator from './AppNavigator';
-// import firebase from 'firebase';
-// Middleware
-import reducers from './reducers';
+// import React, { Component } from 'react';
+// import { AppRegistry } from 'react-native';
+// import { Provider } from 'react-redux';
+// import ReduxThunk from 'redux-thunk';
+// import { createStore, applyMiddleware } from 'redux';
+// import AppNavigator from './AppNavigator';
+// // import firebase from 'firebase';
+// // Middleware
+// import reducers from './reducers';
+//
+// class App extends Component {
+//
+//   render() {
+//     // Wire up redux-thunk
+//     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+//
+//     return (
+//       // STORE
+//       // Pass reducers through createStore call
+//       <Provider store={store}>
+//         <AppNavigator />
+//       </Provider>
+//     );
+//   }
+// }
+//
+// AppRegistry.registerComponent('ARgo', () => App);
 
-class App extends Component {
+import React, { Component } from 'react';
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import { addNavigationHelpers } from 'react-navigation';
+import { AppNavigator } from './Router';
+import reducers from './reducers';
+import AppNavigator from './AppNavigator';
+
+
+const App = ({ dispatch, nav }) => (
+    <AppNavigator
+        screenProps={{ headerStyle: styles.headerStyle }}
+        navigation={addNavigationHelpers({ dispatch, state: nav })}
+    />
+);
+
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+class NavigationalApp extends Component {
 
   render() {
-    // Wire up redux-thunk
-    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-
     return (
-      // STORE
-      // Pass reducers through createStore call
       <Provider store={store}>
         <AppNavigator />
       </Provider>
@@ -24,4 +57,10 @@ class App extends Component {
   }
 }
 
-AppRegistry.registerComponent('ARgo', () => App);
+const styles = {
+    headerStyle: {
+        backgroundColor: '#fff',
+    },
+};
+
+export default NavigationalApp;
