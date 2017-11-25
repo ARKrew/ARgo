bfe10efdd4d6acd09f62874f196642daa1244701
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2017-11-24 16:05:05 -0800

    Fixed headers, removed backbutton

--- src/components/screens/More.js
+++ src/components/screens/More.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { CardSection, Button } from '../common';
+import { View } from 'react-native';
+import { CardSection, Button, Header } from '../common';
 
 class More extends Component {
-  static navigationOptions = {
-    title: 'More'
-  };
-
   render() {
     return (
       <View style={{ flex: 1 }}>
+        <Header headerText={'More'} />
         <CardSection>
           <Button>
             Terms of Service
