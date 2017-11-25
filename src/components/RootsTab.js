2300a27381bfec619ff165320ec1950d324eabe0
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2017-11-24 17:39:42 -0800

    terms of service

--- src/components/RootsTab.js
+++ src/components/RootsTab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image, StyleSheet } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import Profile from './screens/Profile';
 import More from './screens/More';
@@ -26,30 +27,61 @@ const RootsTab = TabNavigator({
     screen: ProfileScreen,
     navigationOptions: {
       header: null,
-      gesturesEnabled: false
+      gesturesEnabled: false,
+      tabBarIcon: () => (
+      <Image
+        source={require('../img/icons/skull.png')}
+        style={[styles.icon]}
+      />
+    ),
     }
   },
   Map: {
     screen: MapScreen,
     navigationOptions: {
       header: null,
-      gesturesEnabled: false
+      gesturesEnabled: false,
+      tabBarIcon: () => (
+      <Image
+        source={require('../img/icons/treasure-map.png')}
+        style={[styles.icon]}
+      />
+    ),
     }
   },
   List: {
     screen: ListScreen,
     navigationOptions: {
       header: null,
-      gesturesEnabled: false
+      gesturesEnabled: false,
+      tabBarIcon: () => (
+      <Image
+        source={require('../img/icons/scroll.png')}
+        style={[styles.icon]}
+      />
+    ),
     }
   },
   More: {
     screen: MoreScreen,
     navigationOptions: {
       header: null,
-      gesturesEnabled: false
+      gesturesEnabled: false,
+      tabBarIcon: () => (
+      <Image
+        source={require('../img/icons/helm.png')}
+        style={[styles.icon]}
+      />
+    ),
     }
   }
+});
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 26,
+    height: 26,
+  },
 });
 
 export default RootsTab;
--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
 import { Header } from '../common';
+import { fbImage } from '../LoginForm';
 
 class Profile extends Component {
   render() {
@@ -12,7 +13,7 @@ class Profile extends Component {
               <Image
                 style={{ width: 350, height: 350 }}
                 source={{
-                    uri: 'https://avatars1.githubusercontent.com/u/28130499?v=4&s=400'
+                    uri: fbImage
                 }}
               />
             </View>
