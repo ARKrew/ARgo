4e382cd7ca6772f6dda6768d6cb7f0804f18091d
ykeanu <ykhernan@ucla.edu>
2017-11-26 17:21:56 -0800

    Comment out map, responsive navigator

--- src/components/screens/Map.js
+++ src/components/screens/Map.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { Header } from '../common';
-import MapViews from '../map/MapView';
+// import MapViews from '../map/MapView';
 
 class Map extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
         <Header headerText={'Map'} />
-        <MapViews />
+        <Text>
+          Hello World! This is a map placeholder.
+        </Text>
       </View>
     );
   }
